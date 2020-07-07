75d5ab3fefcc39c0294985f3951076e83a90a016
mrfajar <rf.cumi@gmail.com>
2020-07-07 08:45:18 +0700

    spand pages about, testimonial, design, price

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -2,9 +2,9 @@
     <div class='contents home'>
         <div class='title'>Selamat datang di momentku</div>
         <p>Yuk, abadikan moment undangan pernikahan mu disini</p>
-		<button class='btn-mail btn-success' data-toggle='modal' data-target='#modal-contact-email'>Buat Sekarang</button>
+		<a class='btn-join btn-success' href='http://localhost/0momentku/MUndangan/daftar'>Buat Sekarang</a>
 	</div>
-	<!-- Modal Contact Email -->
+	<!-- Modal Contact Email
 	<div class='modal fade' id='modal-contact-email' role='dialog'><div class='modal-dialog'>
 	    <div class='modal-content'><div class='modal-body'>
 	        <div class='title'>Kontak Kami</div>
@@ -21,36 +21,12 @@
 	        </form>
 	    </div></div>
 	</div></div>
-	<?php if(isset($_POST['submit-kirim'])){
-	echo "<!-- Modal Contact Email -->
-	<div class='modal fade' id='email-send-success' role='dialog'><div class='modal-dialog'>
-		<div class='modal-content'><div class='modal-body'>
-			<div class='title'>Email Terkirim</div>
-			<p>Email yang ada kirim sudah kami terima, akan kami segera proses. Terima kasih sudah menghubungi dan menggunakan jasa kami</p>
-		</div></div>
-	</div></div>";}?>
+	<?php #if(isset($_POST['submit-kirim'])){
+	#echo "<!-- Modal Contact Email -->
+	#<div class='modal fade' id='email-send-success' role='dialog'><div class='modal-dialog'>
+	#	<div class='modal-content'><div class='modal-body'>
+	#		<div class='title'>Email Terkirim</div>
+	#		<p>Email yang ada kirim sudah kami terima, akan kami segera proses. Terima kasih sudah menghubungi dan menggunakan jasa kami</p>
+	#	</div></div>
+	#</div></div>";}?>-->
 </div>
-<!-- <div class='frame'>
-	<div class='contents about'>
-		<div class='title'>Apa saja yang diberikan di-momentku ?</div>
-		<?php #echo $uc ?>
-	</div>
-</div>
-<div class='frame'>
-	<div class='contents design'>
-		<div class='title'>Beberapa desain jasa kami</div>
-	    <?php #echo $uc ?>
-	</div>
-</div>
-<div class='frame'>
-	<div class='contents price'>
-		<div class='title'>Harga yang kami berikan</div>
-	    <p>Dapat disesuai dengan kebutuan dan budget yang anda miliki, mulai dari Rp NULL - Rp NULL</p>
-	</div>
-</div>
-<div class='frame'>
-	<div class='contents contact'>
-		<div class='title'>Hubungi Kami</div>
-		<?php #echo $uc ?>
-	</div>
-</div>-->
